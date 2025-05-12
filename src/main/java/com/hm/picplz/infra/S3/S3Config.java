b025248d02cddd597ea05333594184ba5f6ea6c1
haeun <bhaeun1105@gmail.com>
2025-05-12 15:19:11 +0900

    [fix] S3 오류 수정
    - s3 Client Bean 생성
    - s3 관련 클래스 생성을 위한 value 작성(secret)
    - s3 접근을 위한 계정 생성

--- src/main/java/com/hm/picplz/infra/S3/S3Config.java
+++ src/main/java/com/hm/picplz/infra/S3/S3Config.java
@@ -7,6 +7,7 @@ import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 
 @Configuration
@@ -30,5 +31,16 @@ public class S3Config {
                 .region(Region.of(region))
                 .credentialsProvider(credentialsProvider)
                 .build();
+    }
+
+    @Bean
+    public S3Client s3Client() {
+        final AwsCredentialsProvider credentialsProvider = StaticCredentialsProvider.create(
+            AwsBasicCredentials.create(accessKey, secretKey)
+        );
+        return S3Client.builder()
+            .region(Region.of(region))
+            .credentialsProvider(credentialsProvider)
+            .build();
     }
 }
