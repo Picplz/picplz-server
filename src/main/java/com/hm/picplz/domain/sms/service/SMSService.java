b1553ca76af333599f88618bba0414e1184929f3
zzinaa <wlsdk2769@gmail.com>
2025-05-20 20:33:51 +0900

    [feat] 휴대폰 본인인증 기능 서비스 구현

---
+++ src/main/java/com/hm/picplz/domain/sms/service/SMSService.java
@@ -0,0 +1,61 @@
+package com.hm.picplz.domain.sms.service;
+
+import lombok.RequiredArgsConstructor;
+import net.nurigo.sdk.NurigoApp;
+import net.nurigo.sdk.message.exception.NurigoMessageNotReceivedException;
+import net.nurigo.sdk.message.model.Message;
+import net.nurigo.sdk.message.service.DefaultMessageService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Random;
+
+@Service
+@RequiredArgsConstructor
+public class SMSService {
+
+    @Value("${coolsms.api.key}")
+    private String apiKey;
+
+    @Value("${coolsms.api.secret}")
+    private String apiSecret;
+
+    @Value("${coolsms.api.number}")
+    private String from;
+
+    private String domain = "https://api.coolsms.co.kr";
+
+    @Transactional
+    public void sendMessage(String to) {
+        DefaultMessageService messageService = NurigoApp.INSTANCE.initialize(apiKey, apiSecret, domain);
+
+        Message message = new Message();
+        String number = generateNumber();
+
+        message.setFrom(from);
+        message.setTo(to);
+        message.setText("픽플즈 핸드폰 본인 인증 번호 [" + number + "]");
+
+        //TODO: number DB에 저장
+
+        try {
+            messageService.send(message);
+        } catch (NurigoMessageNotReceivedException exception) {
+            // 발송에 실패한 메시지 목록을 확인할 수 있습니다!
+            System.out.println(exception.getFailedMessageList());
+            System.out.println(exception.getMessage());
+        } catch (Exception exception) {
+            System.out.println(exception.getMessage());
+        }
+    }
+
+    private String generateNumber() {
+        Random rand = new Random();
+        StringBuilder numStr = new StringBuilder();
+        for (int i = 0; i < 6; i++) {
+            numStr.append(rand.nextInt(10));
+        }
+        return numStr.toString();
+    }
+}
