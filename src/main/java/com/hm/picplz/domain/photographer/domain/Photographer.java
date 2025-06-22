9ce9d4ac5baa1bf48a2ed4989eb1234e6afeb14a
zzinaa <wlsdk2769@gmail.com>
2025-06-22 12:07:53 +0900

    [fix] 작가 생성 메서드 응답 정상적으로 오도록 수정

--- src/main/java/com/hm/picplz/domain/photographer/domain/Photographer.java
+++ src/main/java/com/hm/picplz/domain/photographer/domain/Photographer.java
@@ -66,4 +66,16 @@ public class Photographer extends BaseEntity {
 		photographer.active = YesNo.N;
 		return photographer;
 	}
+
+	public void addAllPhotoMoods(List<PhotoMood> photoMoods) {
+		this.photoMoods.addAll(photoMoods);
+	}
+
+	public void addAllActiveAreas(List<ActiveArea> activeAreas) {
+		this.activeAreas.addAll(activeAreas);
+	}
+
+	public void addAllCameras(List<PhotographerCamera> cameras) {
+		this.cameras.addAll(cameras);
+	}
 }
